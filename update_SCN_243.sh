d831e83080e07eb7e9756610528ede76621e56ea
MutanteGithub <43921676+MutanteGithub@users.noreply.github.com>
2019-01-27 07:23:56 -0300

    Update update_SCN_243.sh

--- update_SCN_243.sh
+++ update_SCN_243.sh
@@ -18,7 +18,7 @@ rm -rf SecureCloud-linux.tar.gz
 
 
 find /root -type f -iname "securecloud.conf"|grep -i ".securecloud"|grep -v "bak"|awk '{print "/usr/local/bin/securecloudd -daemon -conf="substr($0,1,length($0)) " -datadir="substr($0,1,length($0)-16) " -pid="substr($0,1,length($0)-13) "securecloud.pid"}'|sh
-sleep 60
+sleep 90
 find /root -type f -iname "securecloud.conf"|grep -i ".securecloud"|grep -v "bak"|awk '{print "/usr/local/bin/securecloud-cli -conf="substr($0,1,length($0)) " -datadir="substr($0,1,length($0)-16) " masternode start-all 1"}'|sh
 find /root -type f -iname "securecloud.conf"|grep -i ".securecloud"|grep -v "bak"|awk '{print "/usr/local/bin/securecloud-cli -conf="substr($0,1,length($0)) " -datadir="substr($0,1,length($0)-16) " masternode start local"}'|sh
 find /root -type f -iname "securecloud.conf"|grep -i ".securecloud"|grep -v "bak"|awk '{print "/usr/local/bin/securecloud-cli -conf="substr($0,1,length($0)) " -datadir="substr($0,1,length($0)-16) " masternode status"}'|sh
