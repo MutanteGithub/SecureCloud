685169b9e4673276125b13868625c537e611cac6
MutanteGithub <43921676+MutanteGithub@users.noreply.github.com>
2019-01-27 07:39:51 -0300

    Update update_SCN_243.sh

--- update_SCN_243.sh
+++ update_SCN_243.sh
@@ -30,7 +30,7 @@ find /root -type f -iname "securecloud.conf"|grep -i ".securecloud"|grep -v "bak
 find /root -type f -iname "securecloud.conf"|grep -i ".securecloud"|grep -v "bak"|awk '{print "/usr/local/bin/securecloud-cli -conf="substr($0,1,length($0)) " -datadir="substr($0,1,length($0)-16) " masternode status"}'|sh
 
 echo ""
-echo If you see errors when the script tried to activate the Masternodes you can run the following commands again .....
+echo If you see errors when the script tried to activate the Masternodes you can run the following commands again and again if you required .....
 echo ""
 
 echo find /root -type f -iname "securecloud.conf"|grep -i ".securecloud"|grep -v "bak"|awk '{print "/usr/local/bin/securecloud-cli -conf="substr($0,1,length($0)) " -datadir="substr($0,1,length($0)-16) " masternode start-all 1"}'|sh
