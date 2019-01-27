9f6da425bc5092e9425eecb654f8951aa354433a
MutanteGithub <43921676+MutanteGithub@users.noreply.github.com>
2019-01-26 22:43:51 -0300

    Update and rename update_SCN_24.sh to update_SCN_243.sh

--- update_SCN_243.sh
+++ update_SCN_243.sh
@@ -4,7 +4,7 @@ cd ~
 cd /usr/local/bin
 ./securecloud-cli stop
 rm -rf securecloudd securecloud-cli securecloud-tx
-wget https://github.com/securecloudnet/SecureCloud/releases/download/v2.4.0/SecureCloud-linux.tar.gz
+wget https://github.com/securecloudnet/SecureCloud/releases/download/v2.4.3/SecureCloud-linux.tar.gz
 tar -xzf SecureCloud-linux.tar.gz
 rm -rf SecureCloud-linux.tar.gz
 ./securecloudd -daemon
